dff16adfdb67a45b23900e9393c9318674926332
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-01-13 01:59:17 -0600

    assignment started

---
+++ 01-Fundamentals-Part-1/starter/practice.js
@@ -0,0 +1,30 @@
+//Question 1) Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "United States of America";
+let continent = "North America";
+let population = "331,000,000";
+
+//Question 2) Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = "false";
+let language = "";
+
+console.log(country)
+console.log(continent)
+console.log(population)
+console.log(isIsland)
+
+//Question 3 Let, Const, Var
+// 1. Set the value of 'language' to the language spoken where you live (some
+// countries have multiple languages, but just choose one)
+// 2. Think about which variables should be const variables (which values will never
+// change, and which might change?). Then, change these variables to const.
+// 3. Try to change one of the changed variables now, and observe what happens
