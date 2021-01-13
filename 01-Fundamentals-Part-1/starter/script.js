e490a818882e0317866c4c9b5b736d1200a2b9e7
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-01-13 15:49:06 -0600

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,27 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 8);
+// first lecture
 
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 8);
+
+//Second Lecture Data Types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Hannah');
+
+//dynamic typing in action... we do not put 'let' again to change the value of this variable
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+//undefined
+let year;
+console.log(year); //undefined
+console.log(typeof year); //undefined
+
+//dynamic typing... adding a year
+year = 2021;
+console.log(typeof year); //number
