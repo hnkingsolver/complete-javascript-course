5ae0dca7ad9cb032657595136eb54e2d18c4c0cc
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-01-14 15:40:55 -0600

    template literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -40,10 +40,28 @@ age = 25;
 
 var job = 'programmer'
 job = 'gay'
-*/
+
 
 // OPPERATORS
 const ageHannah = 2021 - 1996;
 const ageTanner = 30;
 console.log(ageHannah, ageTanner)
 console.log(ageHannah * 2 / 5)
+*/
+
+//Strings and String Literals
+//a cool thing introduced with es6 is `` aka backticks. you can do things like...\
+const firstName = 'Hannah'
+const greeting = 'enjoy your stay'
+const welcomeMessage = `Hello, my name is ${firstName} and I will be your guide today. ${greeting} and let me know if you need anything.`
+
+console.log(welcomeMessage)
+
+//you can also do things like... instead of having to type /n everytime you want a new line
+const goofin = `Hello
+new line
+new line
+bye.
+`
+
+console.log(goofin)
