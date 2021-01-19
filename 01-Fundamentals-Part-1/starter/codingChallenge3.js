f09ee0f16e18f15862ab4836592c6964bc5af29a
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-01-19 15:25:48 -0600

    Create codingChallenge3.js

---
+++ 01-Fundamentals-Part-1/starter/codingChallenge3.js
@@ -0,0 +1,27 @@
+// const dolphinsAverageScore = (96 + 108 + 89) / 3
+// const koalasAverageScore = (88 + 91 + 110) / 3
+
+// console.log(dolphinsAverageScore, koalasAverageScore)
+
+// if (dolphinsAverageScore === koalasAverageScore) {
+//     console.log("Looks like we have a tie folks!")
+// } else if (dolphinsAverageScore > koalasAverageScore) {
+//     console.log("And the Dolphins win the championship!!")
+// } else {
+//     console.log("And the Koalas win the champiomship!!")
+// }
+
+
+//BONUS 1
+const dolphinsAverageScore = (97 + 112 + 81) / 3
+const koalasAverageScore = (109 + 95 + 86) / 3
+
+console.log(dolphinsAverageScore, koalasAverageScore)
+
+if (dolphinsAverageScore === koalasAverageScore) {
+    console.log("Looks like we have a tie folks!")
+} else if (dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= 100) {
+    console.log("And the Dolphins win the championship!!")
+} else {
+    console.log("And the Koalas win the champiomship!!")
+}
