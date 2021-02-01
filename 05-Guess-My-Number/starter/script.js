0d3eaa23f00af5d981928140d3175a8cc54ee43f
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-02-01 17:03:23 -0600

    added score functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,6 +11,8 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -22,8 +24,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
