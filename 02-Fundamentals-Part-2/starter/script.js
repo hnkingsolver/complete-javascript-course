99f3b5ed89b1ea0fcdcff5e0e0a40fa0af49ec67
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2021-02-25 13:55:09 -0600

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict'; //activate strict mode... has to be very first statement in script file
+"use strict"; //activate strict mode... has to be very first statement in script file
 
 /*
     let hasDriversLicense = false;
@@ -308,23 +308,23 @@ for (let exercise = 1; exercise < 4; exercise++) {
 
 //for comparison
 for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`)
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
 }
 
 //the while loop version of ^^^^
 let rep = 1;
 while (rep <= 10) {
-    console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♂️`)
-    rep++
+  console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♂️`);
+  rep++;
 }
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 console.log(dice);
 
 while (dice !== 6) {
-    console.log(`You rolled a ${dice}`)
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) console.log('Loop is about to end...')
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
 
 //we want to use a forloop when we need a counter like when we loop over an array, but whenever we dont need to do that, the while loop is the way to go.
